da0937aa6e3019bbbfce38a967a623d46c3ad5e8
Alexis Mineaud <alexis@gandi.net>
2014-01-21 11:58:05 +0100

    Comment Add workspace feature for the moment.

--- caliop.ng/src/app/component/dashboard/dashboard.js
+++ caliop.ng/src/app/component/dashboard/dashboard.js
@@ -43,20 +43,22 @@ angular.module('caliop.component.dashboard', [
     }];
 
     $scope.loadContent = function(tab) {
-        $state.go(tab.state);
+        if (tab.state) {
+            $state.go(tab.state);
+        }
     };
 
     $scope.writeMessage = function(tab) {
         $state.go('app.dashboard.writeMessage');
     };
 
-    $scope.addTab = function() {
-        var id = $scope.tabs.length + 1;
-        $scope.tabs.push({
-            id: id,
-            title: "Workspace " + id,
-            content: "Workspace " + id,
-            active: true
-        });
-    };
+    // $scope.addTab = function() {
+    //     var id = $scope.tabs.length + 1;
+    //     $scope.tabs.push({
+    //         id: id,
+    //         title: "Workspace " + id,
+    //         content: "Workspace " + id,
+    //         active: true
+    //     });
+    // };
 }]);
