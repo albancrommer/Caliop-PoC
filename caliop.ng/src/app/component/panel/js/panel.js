9bb19ff479280f332396efb0bbce33931653ec9d
z8po <z8po.contact@gmail.com>
2014-01-21 18:47:20 +0100

    Add template component for left panel

--- caliop.ng/src/app/component/panel/js/panel.js
+++ caliop.ng/src/app/component/panel/js/panel.js
@@ -3,9 +3,23 @@
  */
 angular.module('caliop.component.panel', [
 ])
+.config(function config($stateProvider) {
+    $stateProvider
+        .state('app.panel', {
+            // url: '*',
+            // views:{
+            //     'panel': {
+            //         templateUrl: 'component/panel/panel.tpl.html',
+            //         controller: 'PanelCtrl'
+            //     }
+            // }
+            template: '<h1>Hello</h1>'
+        });
+})
+
 
 .controller('PanelCtrl', [
     function PanelCtrl() {
 
-    // console.log('PanelCtrl');
+    console.log('PanelCtrl is here');
 }]);
