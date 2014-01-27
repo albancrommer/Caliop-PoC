6c6ff29efe9bb4d99853aba889c9ae78aed1787e
Alexis Mineaud <alexis@gandi.net>
2014-01-27 16:07:55 +0100

    Fix account/prefs router config.

--- caliop.ng/src/app/component/account/js/account.js
+++ caliop.ng/src/app/component/account/js/account.js
@@ -16,24 +16,48 @@ angular.module('caliop.component.account', [
         .state('app.account', {
             url: 'account',
             views: {
+                // ui-view="header" of index.html
+                'header@': {
+                    templateUrl: 'component/header/html/header.tpl.html',
+                    controller: 'HeaderCtrl'
+                },
+                // ui-view="layout" of index.html
                 'layout@': {
                     templateUrl: 'component/common/html/fullpage.tpl.html'
                 },
+                // ui-view="main" of fullpage.html
                 'main@app.account': {
                     templateUrl: 'component/account/html/account.tpl.html',
                     controller: 'AccountCtrl'
+                },
+                // ui-view="footer" of index.html
+                'footer@': {
+                    templateUrl: 'component/footer/html/footer.tpl.html',
+                    controller: 'FooterCtrl'
                 }
             }
         })
         .state('app.preferences', {
             url: 'preferences',
             views: {
+                // ui-view="header" of index.html
+                'header@': {
+                    templateUrl: 'component/header/html/header.tpl.html',
+                    controller: 'HeaderCtrl'
+                },
+                // ui-view="layout" of index.html
                 'layout@': {
                     templateUrl: 'component/common/html/fullpage.tpl.html'
                 },
+                // ui-view="main" of fullpage.html
                 'main@app.preferences': {
                     templateUrl: 'component/account/html/preferences.tpl.html',
                     controller: 'PreferencesCtrl'
+                },
+                // ui-view="footer" of index.html
+                'footer@': {
+                    templateUrl: 'component/footer/html/footer.tpl.html',
+                    controller: 'FooterCtrl'
                 }
             }
         });
@@ -45,6 +69,8 @@ angular.module('caliop.component.account', [
 .controller('AccountCtrl', ['$scope', 'auth',
     function AccountCtrl($scope, authSrv) {
 
+    console.log('AccountCtrl');
+
     // $scope.contact = authSrv.getContact();
 }])
 
@@ -53,6 +79,8 @@ angular.module('caliop.component.account', [
  */
 .controller('PreferencesCtrl', ['$scope', 'auth',
     function PreferencesCtrl($scope, authSrv) {
+
+    console.log('PreferencesCtrl');
 
     // console.log('PreferencesCtrl');
 }]);
