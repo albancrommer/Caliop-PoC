d39b491c8e7e64ffafd242c4f85aa1bfbbc27d16
Alexis Mineaud <alexis@gandi.net>
2014-01-28 11:04:34 +0100

    Add sample test for an example.

--- caliop.ng/src/app/app.spec.js
+++ caliop.ng/src/app/app.spec.js
@@ -2,6 +2,24 @@
 
 "use strict";
 
+describe( 'Hello World', function() {
+    var element;
+    var $scope;
+
+    beforeEach(inject(function($compile, $rootScope) {
+        $scope = $rootScope;
+
+        element = angular.element('<div>{{2 + 2}}</div>');
+        element = $compile(element)($rootScope);
+    }));
+
+    it('should be equals to 4', function() {
+        $scope.$digest();
+        expect(element.html()).toBe('4');
+    });
+});
+
+
 describe( 'DashboardCtrl', function() {
     describe( 'isCurrentUrl', function() {
         var AppCtrl, $location, $scope;
