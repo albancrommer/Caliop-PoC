890a86a9774d9f25668d16e46166103a445f6116
Aymeric Barantal <mric@gandi.net>
2014-02-02 13:58:15 +0100

    a simple mda that read mail from first arg at this time

---
+++ caliop/caliop/bin/mda.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import sys
+from cqlengine import connection
+from caliop.config import Configuration
+
+Configuration.load('./tmp/conf.yaml', 'global')
+connection.setup(['127.0.0.1:9160'])
+
+from caliop.mda.agent import DeliveryAgent
+
+
+if __name__ == '__main__':
+    agent = DeliveryAgent(None)
+    data = open(sys.argv[1]).read()
+    messages = agent.process(data)
