e70cf2a6d94e7155c1a1da44c7417e4d121ed544
Aymeric Barantal <mric@gandi.net>
2014-02-02 13:48:15 +0100

    agent processing logic correct

--- caliop/caliop/mda/agent.py
+++ caliop/caliop/mda/agent.py
@@ -1,16 +1,18 @@
 from caliop.mda.message import Message as Mail
-from caliop.core.message import Message
+from caliop.core.message import Message, MessagePart
 
 
 class DeliveryAgent(object):
     """Main logic for delivery of a mail message"""
 
+    exclude_parts = ['multipart/mixed', 'multipart/alternative']
+
     def __init__(self, conf):
         self.conf = conf
 
-    def process_user(self, user, mail):
+    def process_user(self, user, mail, parts):
         # XXX : logic here, for user rules etc
-        return Message.create(user, mail.mail)
+        return Message.create_from_mail(user, mail, parts)
 
     def process(self, buf):
         """
@@ -19,9 +21,18 @@ class DeliveryAgent(object):
         mail = Mail(buf)
 
         messages = []
+        parts = []
+        if mail.parts:
+            for part in mail.parts:
+                if not part.get_content_type() in self.exclude_parts:
+                    part = MessagePart.create(part, mail.users)
+                    part.save()
+                    parts.append(part)
+        print "Parts %r" % ([x.id for x in parts])
         if mail.users:
             for user in mail.users:
-                message = self.process_user(user, mail)
+                print "Processing mail for user %s" % user.id
+                message = self.process_user(user, mail.mail, parts)
                 if message:
                     messages.append(message)
         return messages
