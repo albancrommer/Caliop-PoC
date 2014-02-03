d36df39429a384549dc321a5cc48106d810bb895
Alexis Mineaud <alexis@gandi.net>
2014-02-03 17:42:39 +0100

    Fix entity.base and conversions to dates.

--- caliop.ng/src/app/common/js/entity.base.js
+++ caliop.ng/src/app/common/js/entity.base.js
@@ -12,23 +12,23 @@ angular.module('caliop.common.entity.base', [
     var Base = function Base(obj) {
         var self = this;
 
-        angular.extend(self, obj);
-
         // save obj struct in the object
         angular.forEach(obj, function(value, key) {
             var camelCaseKey = stringSrv.toCamelCase(key);
-            self[camelCaseKey] = value;
+            obj[camelCaseKey] = value;
 
             // remove the dash key
             if (camelCaseKey != key) {
-                delete self[key];
+                delete obj[key];
             }
 
             // convert dates to moment objects
-            if (/^date/.test(key)) {
-                self[key] = moment(self[key]);
+            if (/^date/.test(camelCaseKey)) {
+                obj[camelCaseKey] = moment(obj[camelCaseKey], 'YYYY-MM-DD HH:mm:ss');
             }
         });
+
+        angular.extend(self, obj);
     };
 
     return Base;
