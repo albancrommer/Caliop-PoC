317ac3d90c7c5cc859bada6865f578c0802ff363
Alexis Mineaud <alexis@gandi.net>
2014-02-05 12:40:49 +0100

    Add a e2e test to login and redirect.

---
+++ caliop.ng/protractor/e2e/login.test.js
@@ -0,0 +1,29 @@
+describe('Login and redirect to the inbox', function() {
+  it('should complete the inputs', function() {
+    browser.get('http://localhost:6543/#/');
+
+    element(by.model('credentials.login')).sendKeys('Alexis');
+    element(by.model('credentials.password')).sendKeys('Mineaud');
+  });
+
+  it('should submit the form', function() {
+    element(by.id('submit')).click();
+  });
+
+  it('should redirect to the inbox page', function() {
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:6543/#/inbox');
+
+    // Make sure the cookie is set.
+    browser.manage().getCookie('session').then(function(cookie) {
+        expect(cookie.value).toMatch(/connected/);
+    });
+  });
+
+  it('should create a cookie', function() {
+    browser.manage().getCookie('session').then(function(cookie) {
+        console.log(cookie);
+
+        expect(cookie.value).toMatch(/connected/);
+    });
+  });
+});
