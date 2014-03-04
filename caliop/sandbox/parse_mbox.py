74d08309fcf94231df9f1d6fc52f995e21619a85
Aymeric Barantal <mric@gandi.net>
2014-03-04 09:59:11 +0100

    parse a mbox or a directory with 'eml' files

--- caliop/sandbox/parse_mbox.py
+++ caliop/sandbox/parse_mbox.py
@@ -9,12 +9,15 @@ and import mails
 
 """
 import sys
+import os
+from os import listdir
 from mailbox import mbox
-
+from email.parser import Parser
 from cqlengine import connection
 from caliop.config import Configuration
 Configuration.load('./conf.yaml', 'global')
 
+from caliop.helpers.log import log
 from caliop.core.user import User
 from caliop.core.contact import Contact, ContactLookup
 from caliop.mda.agent import DeliveryAgent
@@ -24,12 +27,23 @@ connection.setup(['127.0.0.1:9160'])
 
 user = sys.argv[1]
 filename = sys.argv[2]
-m = mbox(filename)
+
+if os.path.isdir(filename):
+    mode = 'directory'
+    m = {}
+    files = [f for f in listdir(filename) if \
+        os.path.isfile(os.path.join(filename, f))]
+    parser = Parser()
+    for f in files:
+        m[f] = parser.parse(open('%s/%s' % (filename, f)))
+else:
+    mode = 'mbox'
+    m = mbox(filename)
 
 try:
     user = User.get(user)
 except:
-    print "Creating user"
+    log.info("Creating user")
     user = User.create(id=user,
                        first_name='Test %s' % user.upper(),
                        last_name=user.lower(),
@@ -38,13 +52,15 @@ except:
 
 agent = DeliveryAgent()
 
-for mail in m.values():
+log.info("Processing mode %s" % mode)
+for key, mail in m.iteritems():
     # Create contact for user
+    log.info('Processing mail %s' % key)
     msg = MdaMessage(mail)
     for rec in msg.all_recipients():
         lookup = ContactLookup.get(user, rec)
         if not lookup:
-            print "Creating contact %s" % rec
+            log.info("Creating contact %s" % rec)
             Contact.create(user, {'mail': rec})
     res = agent.process(mail)
-    print "Process result %r" % res
+    log.info("Process result %r" % res)
