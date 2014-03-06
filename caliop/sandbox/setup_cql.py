278413f529a7e066e2c696f750733d430c1bd69d
Aymeric Barantal <mric@gandi.net>
2014-03-06 10:44:10 +0100

    create MessageLookup model in setup_cql

--- caliop/sandbox/setup_cql.py
+++ caliop/sandbox/setup_cql.py
@@ -9,7 +9,7 @@ from cqlengine.management import sync_table
 from caliop.config import Configuration
 Configuration.load('./conf.yaml', 'global')
 from caliop.store import (User, Message, Counter, Contact,
-                          ContactLookup, MessagePart,
+                          ContactLookup, MessagePart, MessageLookup,
                           Thread, ThreadLookup)
 
 connection.setup(['127.0.0.1:9160'])
@@ -17,6 +17,7 @@ connection.setup(['127.0.0.1:9160'])
 sync_table(User)
 sync_table(Message)
 sync_table(MessagePart)
+sync_table(MessageLookup)
 sync_table(Counter)
 sync_table(Contact)
 sync_table(ContactLookup)
