0782b73364d09c8d5bac86ebbd4440aeae01d195
Aymeric Barantal <mric@gandi.net>
2014-03-15 19:11:24 +0100

    setup cql create for tag

--- caliop/caliop/bin/setup_storage.py
+++ caliop/caliop/bin/setup_storage.py
@@ -4,15 +4,16 @@ This script create cassandra models in a local cassandra instance.
 
 This should be abstracted in a backend to get many backend supported.
 """
-from cqlengine import connection
 from cqlengine.management import sync_table
 
+
 def setup_storage():
-    from caliop.store import (User, Message, Counter, Contact,
+    from caliop.store import (User, Message, Counter, Tag, Contact,
                               ContactLookup, MessagePart, MessageLookup,
                               Thread)
 
     sync_table(User)
+    sync_table(Tag)
     sync_table(Message)
     sync_table(MessagePart)
     sync_table(MessageLookup)
