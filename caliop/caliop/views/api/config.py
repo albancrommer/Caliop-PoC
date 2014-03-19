6ed26fb7b2be9bd195c451090352a9606b64ec0b
Guillaume Gauvrit <guillaume@gandi.net>
2014-03-19 10:09:31 +0100

    Use pyramid_kvs for the session management

--- caliop/caliop/views/api/config.py
+++ caliop/caliop/views/api/config.py
@@ -2,10 +2,6 @@
 
 from __future__ import unicode_literals
 
-from pyramid_jinja2 import renderer_factory
-# XXX : use a real session factory
-from pyramid.session import UnencryptedCookieSessionFactoryConfig
-
 from caliop.helpers import renderer
 
 
@@ -13,9 +9,6 @@ def includeme(config):
     """
     Serve a static JSON based REST API.
     """
-    my_session_factory = UnencryptedCookieSessionFactoryConfig('itsaseekreet')
-    config.set_session_factory(my_session_factory)
-
     config.commit()
     config.add_renderer('text_plain', renderer.TextPlainRenderer)
     config.add_renderer('json', renderer.JsonRenderer)
