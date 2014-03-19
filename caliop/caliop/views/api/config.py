f113a14cff2f7155f8efe0ea9224eec852210650
Aymeric Barantal <mric@gandi.net>
2014-03-19 17:05:51 +0100

    pep8 a bit

--- caliop/caliop/views/api/config.py
+++ caliop/caliop/views/api/config.py
@@ -25,7 +25,11 @@ def includeme(config):
     config.scan('caliop.views.api.api')
 
     config.add_route('contact.info', '/api/mock/contact/info')
-    config.add_view('caliop.views.api.ContactInfo', route_name='contact.info', renderer='json')
+    config.add_view('caliop.views.api.ContactInfo',
+                    route_name='contact.info',
+                    renderer='json')
 
     config.add_route('contact.login', '/api/mock/contact/login')
-    config.add_view('caliop.views.api.ContactLogin', route_name='contact.login', renderer='json')
+    config.add_view('caliop.views.api.ContactLogin',
+                    route_name='contact.login',
+                    renderer='json')
